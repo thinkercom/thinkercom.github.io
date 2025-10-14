41c693b99183c20ec7acaa7a8362d0cebcdbfe2a
thinkercom <3241233221@qq.com>
2025-10-14 21:43:15 +0800

    1014-5 index.html ret

--- js/lib/home.js
+++ js/lib/home.js
@@ -1,1 +1,22 @@
-mixins.home={mounted:function o(){var t=this.$refs.homeBackground;var e=t.dataset.images.split(",");var n=Math.floor(Math.random()*e.length);t.style.backgroundImage="url('".concat(e[n],"')");this.menuColor=true},methods:{homeClick:function o(){window.scrollTo({top:window.innerHeight,behavior:"smooth"})}}};
+mixins.home={
+    mounted:function o(){
+        var t=this.$refs.homeBackground;
+        // 检查是否为移动设备
+        var isMobile = window.matchMedia("(max-width: 900px)").matches;
+        // 根据设备类型选择背景图
+        var images = isMobile && t.dataset.mobileImages 
+            ? t.dataset.mobileImages.split(",") 
+            : t.dataset.images.split(",");
+        var n=Math.floor(Math.random()*images.length);
+        t.style.backgroundImage="url('".concat(images[n],"')");
+        this.menuColor=true
+    },
+    methods:{
+        homeClick:function o(){
+            window.scrollTo({
+                top:window.innerHeight,
+                behavior:"smooth"
+            })
+        }
+    }
+};
